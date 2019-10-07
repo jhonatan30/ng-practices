57aeaea03a709cf2930877b2c6a1e8e9fa6f6454
Jhonatan S. Delgado <delgado.jhonatan@gmail.com>
2019-10-07 12:03:31 -0500

    Complement currying section

--- src/app/components/js/curryng/curryng.component.ts
+++ src/app/components/js/curryng/curryng.component.ts
@@ -7,12 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurryngComponent implements OnInit {
 
+  public references: string[] = [
+    'https://www.youtube.com/watch?v=iZLP4qOwY8I'
+  ];
+
   public curryngCodeOne: string = `// add(a, b, c) = add(a)(b)(c);
 const add = x => y => x + y;
 
 add(2)(1); // returns 3
 
 add(2); // returns a function y => 2 + y`;
+
+  public curryngCodeTwo: string = `let dragon = (name, size, element) =>
+name + ' is a ' +
+size + ' dragon that breathes' +
+element + '!'`;
+
+  public curryngCodeThree: string = `let dragon =
+name =>
+  size =>
+  element =>
+    name + ' is a ' +
+    size + ' dragon that breathes ' +
+    element + '!'
+
+console.log(dragon('drogo')('tiny')('ice'));`;
 
   constructor() { }
 
